892b4d34016b70951c1397320ea4a1fa5d83fd4a
costo-kamal <costeau.kamal@gmail.com>
2025-06-10 19:37:19 +0100

    update-7

--- js/products.js
+++ js/products.js
@@ -122,4 +122,250 @@ const products = [
       },
     ],
   },
+  {
+    id: 1,
+    title: "Men's shoes M B",
+    price: "229 DH",
+    description: "iهدا المنتج مصنوع من جلد طبيعي عالي الجودة",
+    colors: [
+      {
+        name: "white",
+        mainImage: "products-image/shoes-m-0.jpeg",
+        thumbnails: [
+          "products-image/shoes-m-3.jpeg",
+          "products-image/shoes-m-1.jpeg",
+          "products-image/shoes-m-4.jpeg",
+          "products-image/shoes-m-5.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "black",
+        mainImage: "products-image/shoes-mg-2.jpeg",
+        thumbnails: [
+          "products-image/shoes-mg-1.jpeg",
+          "products-image/shoes-mg-3.jpeg",
+          "products-image/shoes-mg-4.jpeg",
+          "products-image/shoes-mg-2.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "grey",
+        mainImage: "products-image/shoes-mw-5.jpeg",
+        thumbnails: [
+          "products-image/shoes-mw-1.jpeg",
+          "products-image/shoes-mw-2.jpeg",
+          "products-image/shoes-mw-3.jpeg",
+          "products-image/shoes-mw-4.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Men's shoes M W",
+    price: "229 DH",
+    description: "iهدا المنتج مصنوع من جلد طبيعي عالي الجودة",
+    colors: [
+      {
+        name: "white",
+        mainImage: "products-image/shoes-mw-5.jpeg",
+        thumbnails: [
+          "products-image/shoes-mw-1.jpeg",
+          "products-image/shoes-mw-2.jpeg",
+          "products-image/shoes-mw-3.jpeg",
+          "products-image/shoes-mw-4.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "black",
+        mainImage: "products-image/shoes-m-0.jpeg",
+        thumbnails: [
+          "products-image/shoes-m-3.jpeg",
+          "products-image/shoes-m-1.jpeg",
+          "products-image/shoes-m-4.jpeg",
+          "products-image/shoes-m-5.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "grey",
+        mainImage: "products-image/shoes-mg-1.jpeg",
+        thumbnails: [
+          "products-image/shoes-mg-1.jpeg",
+          "products-image/shoes-mg-3.jpeg",
+          "products-image/shoes-mg-4.jpeg",
+          "products-image/shoes-mg-2.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Men's shoes M G",
+    price: "229 DH",
+    description: "iهدا المنتج مصنوع من جلد طبيعي عالي الجودة",
+    colors: [
+      {
+        name: "grey",
+        mainImage: "products-image/shoes-mg-1.jpeg",
+        thumbnails: [
+          "products-image/shoes-mg-1.jpeg",
+          "products-image/shoes-mg-3.jpeg",
+          "products-image/shoes-mg-4.jpeg",
+          "products-image/shoes-mg-2.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "white",
+        mainImage: "products-image/shoes-mw-5.jpeg",
+        thumbnails: [
+          "products-image/shoes-mw-1.jpeg",
+          "products-image/shoes-mw-2.jpeg",
+          "products-image/shoes-mw-3.jpeg",
+          "products-image/shoes-mw-4.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "black",
+        mainImage: "products-image/shoes-m-0.jpeg",
+        thumbnails: [
+          "products-image/shoes-m-3.jpeg",
+          "products-image/shoes-m-1.jpeg",
+          "products-image/shoes-m-4.jpeg",
+          "products-image/shoes-m-5.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "Men's shoes M B",
+    price: "229 DH",
+    description: "iهدا المنتج مصنوع من جلد طبيعي عالي الجودة",
+    colors: [
+      {
+        name: "white",
+        mainImage: "products-image/shoes-m-0.jpeg",
+        thumbnails: [
+          "products-image/shoes-m-3.jpeg",
+          "products-image/shoes-m-1.jpeg",
+          "products-image/shoes-m-4.jpeg",
+          "products-image/shoes-m-5.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "black",
+        mainImage: "products-image/shoes-mg-2.jpeg",
+        thumbnails: [
+          "products-image/shoes-mg-1.jpeg",
+          "products-image/shoes-mg-3.jpeg",
+          "products-image/shoes-mg-4.jpeg",
+          "products-image/shoes-mg-2.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "grey",
+        mainImage: "products-image/shoes-mw-5.jpeg",
+        thumbnails: [
+          "products-image/shoes-mw-1.jpeg",
+          "products-image/shoes-mw-2.jpeg",
+          "products-image/shoes-mw-3.jpeg",
+          "products-image/shoes-mw-4.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Men's shoes M W",
+    price: "229 DH",
+    description: "iهدا المنتج مصنوع من جلد طبيعي عالي الجودة",
+    colors: [
+      {
+        name: "white",
+        mainImage: "products-image/shoes-mw-5.jpeg",
+        thumbnails: [
+          "products-image/shoes-mw-1.jpeg",
+          "products-image/shoes-mw-2.jpeg",
+          "products-image/shoes-mw-3.jpeg",
+          "products-image/shoes-mw-4.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "black",
+        mainImage: "products-image/shoes-m-0.jpeg",
+        thumbnails: [
+          "products-image/shoes-m-3.jpeg",
+          "products-image/shoes-m-1.jpeg",
+          "products-image/shoes-m-4.jpeg",
+          "products-image/shoes-m-5.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "grey",
+        mainImage: "products-image/shoes-mg-1.jpeg",
+        thumbnails: [
+          "products-image/shoes-mg-1.jpeg",
+          "products-image/shoes-mg-3.jpeg",
+          "products-image/shoes-mg-4.jpeg",
+          "products-image/shoes-mg-2.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Men's shoes M G",
+    price: "229 DH",
+    description: "iهدا المنتج مصنوع من جلد طبيعي عالي الجودة",
+    colors: [
+      {
+        name: "grey",
+        mainImage: "products-image/shoes-mg-1.jpeg",
+        thumbnails: [
+          "products-image/shoes-mg-1.jpeg",
+          "products-image/shoes-mg-3.jpeg",
+          "products-image/shoes-mg-4.jpeg",
+          "products-image/shoes-mg-2.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "white",
+        mainImage: "products-image/shoes-mw-5.jpeg",
+        thumbnails: [
+          "products-image/shoes-mw-1.jpeg",
+          "products-image/shoes-mw-2.jpeg",
+          "products-image/shoes-mw-3.jpeg",
+          "products-image/shoes-mw-4.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+      {
+        name: "black",
+        mainImage: "products-image/shoes-m-0.jpeg",
+        thumbnails: [
+          "products-image/shoes-m-3.jpeg",
+          "products-image/shoes-m-1.jpeg",
+          "products-image/shoes-m-4.jpeg",
+          "products-image/shoes-m-5.jpeg",
+        ],
+        sizes: ["40", "41", "42", "43"],
+      },
+    ],
+  },
 ]; // -----------------End------------------
